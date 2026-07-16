63e9d36bd976ac32cf2e657487f7edd934af1b30
Sho Sakaue <sho.sakaue0808@gmail.com>
2026-07-15 22:18:36 -0600

    cleaned up

--- src/bayesbench/visualization/plot_branin.py
+++ src/bayesbench/visualization/plot_branin.py
@@ -7,8 +7,30 @@ def main() -> None:
     seed = 0
     input_path_gp_ei = f"results/branin/branin_gp_ei_{seed}.csv"
     input_path_random_search = f"results/branin/branin_random_search_{seed}.csv"
-    input_paths = [input_path_gp_ei, input_path_random_search]
+
+    output_path = f"results/branin/branin_gp_ei_{seed}.png"
+    plot_best_so_far(
+        input_path=input_path_gp_ei,
+        output_path=output_path,
+        title="Branin Optimization: Gaussian Process Expected Improvement",
+        seed=seed
+    )
+
+    print(f"Saved plot to: {output_path}")
+
+    output_path = f"results/branin/branin_random_search_{seed}.png"
+
+    plot_best_so_far(
+        input_path=input_path_random_search,
+        output_path=output_path,
+        title="Branin Optimization: Random Search",
+        seed=seed
+    )
+
+    print(f"Saved plot to: {output_path}")
+
     output_path = f"results/branin/branin_multi_opt_{seed}.png"
+    input_paths = [input_path_gp_ei, input_path_random_search]
 
     plot_multi_opt_best_so_far(
         input_paths=input_paths,
@@ -16,25 +38,6 @@ def main() -> None:
         title=f"Branin Optimization: Multiple Optimizers",
         seed=seed
     )
-
-    # plot_best_so_far(
-    #     input_path=input_path,
-    #     output_path=output_path,
-    #     title="Branin Optimization: Gaussian Process Expected Improvement",
-    #     seed=seed
-    # )
-
-    # print(f"Saved plot to: {output_path}")
-
-    # input_path = f"results/branin/branin_random_search_{seed}.csv"
-    # output_path = f"results/branin/branin_random_search_{seed}.png"
-
-    # plot_best_so_far(
-    #     input_path=input_path,
-    #     output_path=output_path,
-    #     title="Branin Optimization: Random Search",
-    #     seed=seed
-    # )
 
     print(f"Saved plot to: {output_path}")
 
